c9ad9a1e9be7e15da50ae290b2b54ca06981f5b9
Your Name <cjw269@cornell.edu>
2026-08-02 21:07:57 -0400

    fix: load Firebase config from environment

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,19 +5,21 @@ import { getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+import env from './env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4KUrD-rgQTXSmsqlVCtm1srvb_PYYAak",
-  authDomain: "north-fork-tourism-fdf6a.firebaseapp.com",
-  projectId: "north-fork-tourism-fdf6a",
-  storageBucket: "north-fork-tourism-fdf6a.firebasestorage.app",
-  messagingSenderId: "1020241594344",
-  appId: "1:1020241594344:web:bc9063d74686b01ba4465d",
-  measurementId: "G-BDDQ4BBGLE"
+  apiKey: env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- env.ts
+++ env.ts
@@ -7,7 +7,17 @@ const env = createEnv({
   client: {
     // API URL for the backend service
     // This is used for generating the API client with the "gen-api" command
-    EXPO_PUBLIC_API_URL: z.string().url('API_URL must be a valid URL'),
+    EXPO_PUBLIC_API_URL: z.string().url('API_URL must be a valid URL').optional(),
+
+    EXPO_PUBLIC_FIREBASE_API_KEY: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_PROJECT_ID: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_APP_ID: z.string().min(1),
+    EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID: z.string().optional(),
+
+    EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN: z.string().optional(),
   },
 
   /**
@@ -15,6 +25,24 @@ const env = createEnv({
    */
   runtimeEnv: {
     EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+
+    EXPO_PUBLIC_FIREBASE_API_KEY:
+      process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    EXPO_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET:
+      process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    EXPO_PUBLIC_FIREBASE_APP_ID:
+      process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID:
+      process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
+    EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN:
+      process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN,
   },
 });
 
